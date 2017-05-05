12343be07b28981b4c35efcb18d03eef404178bc
Norberto Vasconcelos <norberto.vasconcelos@forkit.pt>
2017-05-05 11:09:42 +0100

    Initial Commit

---
+++ NVFileDownloader/NVFileDownloader.h
@@ -0,0 +1,19 @@
+//
+//  NVFileDownloader.h
+//  NVFileDownloader
+//
+//  Created by Norberto Vasconcelos on 05/05/2017.
+//  Copyright © 2017 Norberto Vasconcelos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NVFileDownloader.
+FOUNDATION_EXPORT double NVFileDownloaderVersionNumber;
+
+//! Project version string for NVFileDownloader.
+FOUNDATION_EXPORT const unsigned char NVFileDownloaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NVFileDownloader/PublicHeader.h>
+
+
